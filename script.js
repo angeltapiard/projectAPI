e4fa66081967756cc1c49025ee4a49b65e610bda
ANGEL MIGUEL TAPIA <O99ZZ92@uasd.local>
2024-05-31 07:36:04 -0600

    Actualizacion del js para mostrar los personajes

--- script.js
+++ script.js
@@ -13,20 +13,30 @@ async function fetchCharacters() {
 async function displayCharacters() {
     const characters = await fetchCharacters();
     const appContainer = document.getElementById('app');
+    const rowsContainer = document.createElement('div');
+    rowsContainer.classList.add('rows-container');
 
-    characters.forEach(character => {
+    let row;
+    characters.forEach((character, index) => {
+        // Crear una nueva fila cada 5 personajes
+        if (index % 5 === 0) {
+            row = document.createElement('div');
+            row.classList.add('character-row');
+            rowsContainer.appendChild(row);
+        }
+
         const characterCard = document.createElement('div');
         characterCard.classList.add('character-card');
         characterCard.innerHTML = `
-        <img src="${character.image}" alt="${character.name}">
+            <img src="${character.image}" alt="${character.name}">
             <h3>${character.name}</h3>
             <p>Status: ${character.status}</p>
             <p>Species: ${character.species}</p>
-           
         `;
-        appContainer.appendChild(characterCard);
+        row.appendChild(characterCard);
     });
+
+    appContainer.appendChild(rowsContainer);
 }
 
 displayCharacters();
-
